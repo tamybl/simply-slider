97989db14ad1914a85cc13d966a7ba8d74dc6f00
tamybl <tamara.bravo.l@gmail.com>
2018-11-06 12:10:19 -0300

    fix event listener

--- js/main.js
+++ js/main.js
@@ -1,40 +1,44 @@
-var nextBtn = document.querySelector(".gallery .buttons .next");
-var prevBtn = document.querySelector(".gallery .buttons .prev");
-var slide = document.querySelectorAll(".gallery .photos .block");
-var i = 0;
+simplySlider = () => {
+    var nextBtn = document.querySelector(".gallery .buttons .next");
+    var prevBtn = document.querySelector(".gallery .buttons .prev");
+    var slide = document.querySelectorAll(".gallery .photos .block");
+    var i = 0;
 
-function sliderAuto(time) {
-    setInterval(next, time);
-};
+    function sliderAuto(time) {
+        setInterval(next, time);
+    };
 
 
-prevBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    previous();
-});
+    prevBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        previous();
+    });
 
-nextBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    next();
-});
+    nextBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        next();
+    });
 
-function previous() {
-    slide[i].classList.remove("active");
-    i--;
-    if (i < 0) {
-        i = slide.length - 1;
+    function previous() {
+        slide[i].classList.remove("active");
+        i--;
+        if (i < 0) {
+            i = slide.length - 1;
+        }
+        slide[i].classList.add("active");
     }
-    slide[i].classList.add("active");
+
+
+    function next() {
+        slide[i].classList.remove("active");
+        i++;
+        if (i >= slide.length) {
+            i = 0;
+        }
+        slide[i].classList.add("active");
+    }
+
+    sliderAuto(2500);
 }
 
-
-function next() {
-    slide[i].classList.remove("active");
-    i++;
-    if (i >= slide.length) {
-        i = 0;
-    }
-    slide[i].classList.add("active");
-}
-
-sliderAuto(2500);
+document.addEventListener('DOMContentLoaded', simplySlider);
